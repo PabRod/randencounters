9e02599e7f45b2bd683a1ce6f7441e385a0a467c
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2021-03-10 20:10:21 +0100

    Estimate the container size analytically

--- R/auxs.R
+++ R/auxs.R
@@ -8,14 +8,16 @@ get_one <- function(names_location = 'data/list.txt') {
   return(chosen_one)
 }
 
-make_groups <- function(names_location = 'data/list.txt', group_size = 2) {
+make_groups <- function(names_location = 'data/list.txt', group_size = 2, padding = '') {
   # Load the list of people
   people <- readLines(names_location)
 
-  # Pad the ungrouped ones with NA
+  # Use the remainder to get the size of the container
   # If the group size is not an integer divisor of the amount of people, some
   # groups will need the addition of dummy members
-  while(length(people) %% group_size != 0) people <- c(people, 'NA')
+  container_size <- group_size + group_size * (length(people) - 1) %/% group_size
+  # Pad the ungrouped ones with NA
+  people <- c(people, rep(padding, container_size - length(people)))
 
   # Shuffle and arrange as a matrix of the desired size
   shuffled <- sample(people)
